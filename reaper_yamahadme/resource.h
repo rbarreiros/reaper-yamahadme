3957767e8f4ddb6596e158a44c24780e7db70d38
Rui Barreiros <rbarreiros@gmail.com>
2012-03-10 19:40:06 +0000

    - changed back to res.rc type of resource now using ResEdit, all because   of damn Mac port....

--- reaper_yamahadme/resource.h
+++ reaper_yamahadme/resource.h
@@ -1,22 +1,11 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by res.rc
-//
-#define IDD_SURFACEEDIT                 265
-#define IDC_COMBO2                      1001
-#define IDC_COMBO3                      1002
-#define IDC_RADIO1						1003
-#define IDC_RADIO2						1004
-#define IDC_RADIO3						1005
-#define IDC_CHKB1						1006
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#define IDD_SURFACEEDIT                         265
+#define IDC_COMBO2                              1001
+#define IDC_COMBO3                              1002
+#define IDC_RADIO1                              1003
+#define IDC_RADIO2                              1004
+#define IDC_RADIO3                              1005
+#define IDC_CHKB1                               1007
